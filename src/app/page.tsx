d40e1fb897a2ef906740eedd18823a2bfa904374
nelsonhomo <nelson.homo@csaude.org.mz>
2023-07-14 08:35:44 +0200

    SESP-WEB: Initial project struture

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <h1 className="text-blue-400 text-7xl">SESP WEB - The new interface for the OpenMRS Platform</h1>
+    </main>
+  )
+}
